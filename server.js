389eea01e8d8a14e937de97227109279cbae30a8
SANGDONG <csd9969@gmail.com>
2023-11-23 01:50:30 +0900

    tomorrow

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import { v4 as uuidv4 } from "uuid";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const app = express();
 const PORT = 8080;
@@ -15,41 +16,49 @@ app.listen(PORT, function () {
   console.log(`listening on ${PORT}`);
 });
 
-function generateSessionID() {
-  return uuidv4();
-}
+//세션에 id저장
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET, // 세션을 암호화하기 위한 키. 실제 개발에선 보안을 위해 .env 파일 등에 저장하는 것이 좋습니다.
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
 //로그인
 app.post("/login", async (req, res) => {
   const { id, password } = req.body;
 
-  const user = await User.findOne({ id: id });
+  const user = await User.findOne({ id });
   const ok = await bcrypt.compare(password, user.password);
   if (!user || !ok) {
     return res.status(401).send("아이디 또는 비밀번호가 잘못되었습니다.");
   }
 
-  const sessionID = generateSessionID(); // 세션 ID 생성 함수
-  res.cookie("session_id", sessionID, { httpOnly: true });
-  res.status(200).send("로그인 성공");
+  // res.cookie("session_id", sessionID, { httpOnly: true });
+  req.session.user = user;
+  return res.status(200).send("로그인 성공");
 });
 
 //쿠키 확인
-app.get("/schedules", async (req, res) => {
-  // 쿠키를 통해 사용자 식별
-  const sessionID = req.cookies["session_id"];
-  const user = findUserBySessionID(sessionID);
+// app.get("/schedules", async (req, res) => {
+//   // 쿠키를 통해 사용자 식별
+//   const sessionID = req.cookies["session_id"];
+//   const user = findUserBySessionID(sessionID);
 
-  // 사용자의 일정 데이터를 가져옵니다.
-  const schedules = await getScheduleData(user.id);
-  res.json(schedules);
-});
+//   // 사용자의 일정 데이터를 가져옵니다.
+//   const schedules = await getScheduleData(user.id);
+//   res.json(schedules);
+// });
 
 //로그아웃
 app.post("/logout", (req, res) => {
   // 쿠키를 삭제합니다.
-  res.clearCookie("session_id");
+  // res.clearCookie("session_id");
 
   // 로그아웃 성공 메시지를 클라이언트에게 전송합니다.
+  req.session.destroy();
   res.status(200).send("로그아웃 성공");
 });
 
